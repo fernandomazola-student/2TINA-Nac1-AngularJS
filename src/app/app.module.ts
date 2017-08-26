bc97ac63f2b7c62dfa8d1fe67c24f794e983e408
Fernando Mazola <fernando_mazola@hotmail.com>
2017-08-26 01:09:37 -0300

    Instalado type script, angular/cli, criado o projeto ng new, adicionando pacotes bootstrap, jquery e components da nac. Finalizado também a header e iniciando a home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { ContatosComponent } from './contatos/contatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    AlunosComponent,
+    DisciplinasComponent,
+    ContatosComponent
   ],
   imports: [
     BrowserModule
